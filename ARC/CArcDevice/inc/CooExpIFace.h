7cf43a862136e8476c4159836450d542974fadc8
David Hale <dhale@caltech.edu>
2023-08-30 11:21:47 -0700

    Add ARC API to Software tree (was previously external, in /opt)
    dh

---
+++ ARC/CArcDevice/inc/CooExpIFace.h
@@ -0,0 +1,42 @@
+#ifndef _ARC_COOEXPIFACE_H_
+#define _ARC_COOEXPIFACE_H_
+
+#include <cstdint>
+
+#include <CArcDeviceDllMain.h>
+
+
+namespace arc
+{
+	namespace gen3
+	{
+
+		class GEN3_CARCDEVICE_API CooExpIFace   // COO Expose Interface Class
+		{
+			public:
+
+				virtual ~CooExpIFace( void ) = default;
+
+				virtual void exposeCallback( int devnum, std::uint32_t uiElapsedTime, std::uint32_t ExposureTime ) = 0;
+
+				virtual void readCallback( int devnum, std::uint32_t uiPixelCount, std::uint32_t uiImageSize ) = 0;
+
+                                virtual void frameCallback( int devnum,                         // device number
+                                                            std::uint32_t   uiFramesPerBuffer,  // Frames-per-buffer count
+                                                            std::uint32_t   uiFrameCount,       // PCI frame count
+                                                            std::uint32_t   uiRows,             // # of rows in frame
+                                                            std::uint32_t   uiCols,             // # of cols in frame
+                                                            void* pBuffer ) = 0;                // Pointer to frame start in buffer
+
+                                virtual void ftCallback( int devnum ) = 0;
+
+			protected:
+
+				CooExpIFace( void ) = default;
+		};
+
+	}	// end gen3 namespace
+}	// end arc namespace
+
+
+#endif	// _ARC_COOEXPIFACE_H_
