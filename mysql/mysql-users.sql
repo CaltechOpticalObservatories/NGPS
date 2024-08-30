45ad79a6b015e34bf0394989eb9285f0991568ee
Chaz Shapiro <charles.a.shapiro@jpl.nasa.gov>
2024-08-30 11:13:56 -0700

    updated mysql-users.sql

--- mysql/mysql-users.sql
+++ mysql/mysql-users.sql
@@ -15,27 +15,30 @@ GRANT EXECUTE ON db.* TO user@'%';
 */
 
 -- grafana
+DROP USER IF EXISTS grafana@localhost;
 CREATE USER grafana@localhost IDENTIFIED BY 'Write1Spec2.!';
 GRANT SELECT on telemetry.* TO grafana@localhost;
 SHOW GRANTS FOR grafana@localhost;
 
 -- obseq
+DROP USER IF EXISTS obseq@localhost;
 CREATE USER obseq@localhost IDENTIFIED BY 'Write1Spec2.!';
 GRANT SELECT on ngps.target_sets TO obseq@localhost;
 GRANT SELECT, UPDATE, INSERT on ngps.targets TO obseq@localhost;
-GRANT INSERT on ngps.completed_observations TO obseq@localhost; -- DEPRECATED
 GRANT INSERT ON ngps.completed_obs TO obseq@localhost;
 SHOW GRANTS FOR obseq@localhost;
 
 -- gui
+DROP USER IF EXISTS gui@localhost;
 CREATE USER gui@localhost IDENTIFIED BY 'Write1Spec2.!';
 GRANT ALL ON ngps.owner TO gui@localhost;
 GRANT ALL ON ngps.targets TO gui@localhost;
 GRANT ALL ON ngps.target_sets TO gui@localhost;
-GRANT SELECT ON ngps.completed_observations TO gui@localhost;
+GRANT SELECT ON ngps.completed_obs TO gui@localhost;
 SHOW GRANTS FOR gui@localhost;
 
 -- telemlogger
+DROP USER IF EXISTS telemlogger@localhost;
 CREATE USER telemlogger@localhost IDENTIFIED BY 'Password-123';
 GRANT CREATE, INSERT ON telemetry.* TO telemlogger@localhost;
 SHOW GRANTS FOR telemlogger@localhost;
