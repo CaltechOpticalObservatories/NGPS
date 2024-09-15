de1c203813352cce4782b3182724a23e3895508d
Chaz Shapiro <charles.a.shapiro@jpl.nasa.gov>
2024-09-15 11:08:54 -0700

    Tweaked mysql/reset-password.sh

--- mysql/reset-password.sh
+++ mysql/reset-password.sh
@@ -2,10 +2,12 @@
 
 # Script to reset a user's password in NGPS GUI -- not to be confused with mysql users
 # mySQL login will be for user=gui, not the same as user being deleted
+# The new password will not be encrypted but will still work in the GUI.
+# User should log in to the GUI using the new password then change it (then it will be encrypted)
 
 user=$1
 admin=gui
-newpassword=RESET123
+newpassword=RESET
 
 # -z checks if var is empty
 if [ -z "$user" ]; then
