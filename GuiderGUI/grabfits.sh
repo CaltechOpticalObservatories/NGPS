f38e0eb60d0ab028ac4912ff3eaf9af373db1501
Chaz Shapiro <charles.a.shapiro@jpl.nasa.gov>
2024-11-20 10:35:41 -0800

    Cleaned up grab fits button behavior for ACAM and SCAMs

--- GuiderGUI/grabfits.sh
+++ GuiderGUI/grabfits.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Save FITS file and push to another DS9 window
+# Use --no-display to skip pushing to DS9
 
 camera=$1  # guider or slicev
 saveopt=$2 # push display or save only
@@ -12,7 +13,17 @@ newID=${id}_grabs
 fname=${grabdir}/${id}_`date +"%Y%m%d-%H%M%S"`.fits
 DEFzoom=.5
 
-xpaset -p $id save mosaicimage $fname  # save the FITS file
+# xpa save and push commands are different for ACAM and SCAMs
+if [[ "$camera" == "guider" ]]; then
+fitstype="fits"
+savetype=""
+else
+fitstype="mosaicimage wcs"
+savetype="mosaicimage"
+fi
+
+# Save the FITS file
+xpaset -p $id save $savetype $fname
 echo $fname
 
 # Quit after saving if --no-display
@@ -27,7 +38,7 @@ process_running=`ps aux | grep ds9 | grep " $newID "`  # should be empty string 
 if xpaget $newID xpa connect; then
 
 	xpaset -p $newID frame new && error=1  # error=1 if there's an error
-	cat $fname | xpaset $newID mosaicimage wcs
+	cat $fname | xpaset $newID $fitstype #mosaicimage wcs
 
 # If the process is running (string is not empty), show linux message
 elif [ -n "$process_running" ]; then
@@ -37,9 +48,9 @@ elif [ -n "$process_running" ]; then
 
 # Otherwise, start new ds9 with the saved FITS file
 else
-	
-	nohup ds9 -mosaicimage wcs $fname -title $newID -mode none -tile yes \
-	  -zoom $DEFzoom -scale linear -scale mode zscale \
+#datasec hides the overlapping parts of SCAMs	
+	nohup ds9 -$fitstype $fname -title $newID -mode none -tile yes \
+	  -zoom $DEFzoom -scale linear -scale mode zscale -scale datasec yes \
 	  -view object no -view colorbar no \
 	   >&- 2>&- &  # this stuff "closes stdout" to make ds9 parent DS9 process stop waiting for child DS9 to finish
 	   				# https://stackoverflow.com/questions/20338162/how-can-i-launch-a-new-process-that-is-not-a-child-of-the-original-process
