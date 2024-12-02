1abab7d74098dcc2bfdc3b5a94d3b085477a88eb
Prakriti Gupta <pgupta@caltech.edu>
2024-12-01 21:12:07 -0800

    add refresh

--- pygui/layout_service.py
+++ pygui/layout_service.py
@@ -804,14 +804,6 @@ class LayoutService:
         # Create the Refresh Button
         self.refresh_button = QPushButton("Refresh Table")
         self.refresh_button.setStyleSheet("""
-            QPushButton {
-                background-color: #4CAF50;  /* Green background */
-                color: white;
-                font-weight: bold;
-                padding: 10px;
-                border: none;
-                border-radius: 5px;
-            }
             QPushButton:hover {
                 background-color: #388E3C;
             }
@@ -819,14 +811,13 @@ class LayoutService:
                 background-color: #2C6B2F;
             }
         """)
-        self.refresh_button.clicked.connect(self.refresh_table)  # Connect the refresh button to the refresh_table method
+        self.refresh_button.clicked.connect(self.logic_service.refresh_table)  # Connect the refresh button to the refresh_table method
 
-        # Add the label and refresh button to the row1_layout
-        row1_layout.addWidget(self.target_name_label)
-        row1_layout.addWidget(self.refresh_button)
+        # # Add the label and refresh button to the row1_layout
+        # row1_layout.addWidget(self.refresh_button)
 
         # Add the entire row_widget (Row 1 + Row 2) to the control_layout
-        row_layout.addLayout(row1_layout)
+        row_layout.addWidget(self.target_name_label)
 
         # Add label and input fields (Exposure Time and Slit Width) to Row 2
         row2_layout = QHBoxLayout()
@@ -846,6 +837,7 @@ class LayoutService:
         row2_layout.addWidget(self.exposure_time_box)
         row2_layout.addWidget(self.slit_width_label)
         row2_layout.addWidget(self.slit_width_box)
+        row2_layout.addWidget(self.refresh_button)
 
         # Add the Exposure Time and Slit Width layout to the main layout
         row_layout.addLayout(row2_layout)
