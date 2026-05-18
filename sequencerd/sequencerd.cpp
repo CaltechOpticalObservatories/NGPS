e0071770dd48d40997a7f8b56be96d47fa86d359
David Hale <dhale@astro.caltech.edu>
2026-05-18 08:26:02 -0700

    bugfix: CAL target should not request TCS move (#423)
    bugfix: sequencerd missed camerad's published status

--- sequencerd/sequencerd.cpp
+++ sequencerd/sequencerd.cpp
@@ -141,6 +141,7 @@ int main(int argc, char **argv) {
 
   std::this_thread::sleep_for( std::chrono::milliseconds(200) );
   sequencerd.sequence.publish_snapshot();
+  sequencerd.sequence.request_snapshot();
 
   // This will pre-thread N_THREADS threads.
   // The 0th thread is reserved for the blocking port, and the rest are for the non-blocking port.
